ae0f6afe0d3c4ce85f364731a2fe6ebcac2bc812
madyanalj <22075444+madyanalj@users.noreply.github.com>
2020-04-12 22:21:44 +0100

    tests: remove unneeded mocks

--- __mocks__/monaco-editor/index.ts
+++ __mocks__/monaco-editor/index.ts
@@ -1,10 +1,2 @@
-export const editor = { create: jest.fn() };
-
-export const languages = {
-  typescript: {
-    getTypeScriptWorker: jest.fn(),
-  },
-};
-
 export const KeyMod = {};
 export const KeyCode = {};
